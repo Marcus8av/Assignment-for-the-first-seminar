f8548298a0a2291fdd894657e1aea9c1e0777c86
markus b <mnogo_bukof@mail.ru>
2022-10-16 23:41:58 +0700

    четвертое задание

---
+++ задание4/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принимает число (N), 
+а на выходе показывает все чётные числа от 1 до N.*/
+
+// Запрос числа от пользователя
+Console.Write("Введите число :");
+int limit = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(limit);
+int count = 1;
+
+while (count < limit)
+{
+    count++;
+    if (count % 2 == 0)
+    Console.WriteLine(count);
+}
